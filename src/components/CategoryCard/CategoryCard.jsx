42624e889bacfd55c2457b601657bbf22c6863af
Dmitry Bykov <bykovdmnic@outlook.com>
2025-11-15 22:05:48 +0300

    компонент Badge

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -1,35 +1,42 @@
 import './CategoryCard.scss'
-import {Image} from "minista";
-import Icon from "@/components/Icon";
+import { Image } from 'minista'
+import Icon from '@/components/Icon'
+import Badge from '@/components/Badge'
 
 const CategoryCard = (props) => {
   const {
-    className,
     title,
     images = [],
+    badge,
   } = props
 
   return (
-    <a
-      className='category-card'
-      href='/movies'
-    >
-      <div className='category-card__images'>
+    <a className="category-card" href="/movies">
+      <div className="category-card__images">
         {images.map((imgSrc, index) => (
           <Image
-            className='category-card__image'
+            className="category-card__image"
             src={imgSrc}
             key={index}
           />
         ))}
       </div>
-      <div className='category-card__body'>
-        <h3 className='category-card__title'>
-          {title}
+      <div className="category-card__body">
+        <h3 className="category-card__title">
+          {badge && (
+            <Badge
+              className="category-card__badge"
+              mode="accent"
+              isBig
+            >
+              {badge}
+            </Badge>
+          )}
+          <span>{title}</span>
         </h3>
         <Icon
-          className='category-card__icon'
-          name='arrow-right'
+          className="category-card__icon"
+          name="arrow-right"
         />
       </div>
     </a>
--- src/sections/Collections/Collections.jsx
+++ src/sections/Collections/Collections.jsx
@@ -1,26 +1,24 @@
 import './Collections.scss'
-import classNames from 'classnames'
-import Tabs from "@/components/Tabs";
-import collectionGroups from "@/sections/Collections/collectionGroups";
-import getIdFromTitle from "@/utils/getidFromTitle";
-import Section from "@/layouts/Section";
-import SliderNavigation from "@/components/Slider/components/SliderNavigation";
-import Slider from "@/components/Slider";
-import CategoryCard from "@/components/CategoryCard";
+import Tabs from '@/components/Tabs'
+import collectionGroups from './collectionGroups'
+import getIdFromTitle from '@/utils/getIdFromTitle'
+import Section from '@/layouts/Section'
+import SliderNavigation from '@/components/Slider/components/SliderNavigation'
+import Slider from '@/components/Slider'
+import CategoryCard from '@/components/CategoryCard'
 
 const Collections = () => {
-
   return (
     <Tabs
-      className='collections container'
-      title='collections'
+      className="collections container"
+      title="collections"
       isEnableOnlyOnMobile
       items={collectionGroups.map((collectionGroup) => ({
         isActive: collectionGroup.isActive,
         title: collectionGroup.title,
         children: (
-          <div className='collections__group'>
-            <p className='collections__title hidden-mobile'>
+          <div className="collections__group">
+            <p className="collections__title hidden-mobile">
               {collectionGroup.title}
             </p>
             {collectionGroup.items.map((collectionItem, index) => {
@@ -30,19 +28,19 @@ const Collections = () => {
                 sliderParams,
               } = collectionItem
 
-              const titleFormated = `${getIdFromTitle(collectionGroup.title)}-${getIdFromTitle(title)}`
-              const titleId = `${titleFormated}-title`
-              const sliderNavigationId = `${titleFormated}-slider-navigation`
+              const titleFormatted = `${getIdFromTitle(collectionGroup.title)}-${getIdFromTitle(title)}`
+              const titleId = `${titleFormatted}-title`
+              const sliderNavigationId = `${titleFormatted}-slider-navigation`
 
               return (
                 <Section
-                  className='collections__section'
+                  className="collections__section"
                   title={title}
                   titleId={titleId}
                   actions={(
                     <SliderNavigation
                       id={sliderNavigationId}
-                      mode='tile'
+                      mode="tile"
                     />
                   )}
                   isActionsHiddenOnMobile
@@ -50,7 +48,7 @@ const Collections = () => {
                 >
                   <Slider
                     sliderParams={sliderParams}
-                    navigationTargetElementTd={sliderNavigationId}
+                    navigationTargetElementId={sliderNavigationId}
                     isBeyondTheViewportOnMobileS
                   >
                     {categoryItems.map((categoryItem, index) => (
