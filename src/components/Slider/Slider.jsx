f70657c4d4bb5d7ef78f7dcd9660b2158ca93c6a
Dmitry Bykov <bykovdmnic@outlook.com>
2025-11-18 21:13:06 +0300

    стилизовал ReviewCard

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -45,6 +45,7 @@ const Slider = (props) => {
      * ' ' | default | 'abs-bottom'
      */
     navigationPosition = ' ',
+    navigationMode,
     isNavigationHiddenMobile = true,
   } = props
 
@@ -77,6 +78,7 @@ const Slider = (props) => {
       {!navigationTargetElementId && (
         <SliderNavigation
           className='slider__navigation'
+          mode={navigationMode}
           position={navigationPosition}
           isHiddenMobile={isNavigationHiddenMobile}
         />
--- src/sections/MovieDetails/MovieDetails.jsx
+++ src/sections/MovieDetails/MovieDetails.jsx
@@ -1,6 +1,9 @@
 import './MovieDetails.scss'
 import SliderNavigation from "@/components/Slider/components/SliderNavigation";
 import Slider from "@/components/Slider";
+import PersonCard from "@/components/PersonCard";
+import Button from "@/components/Button";
+import ReviewCard from "@/components/ReviewCard";
 
 const MovieDetails = (props) => {
   const {
@@ -9,6 +12,92 @@ const MovieDetails = (props) => {
 
   const titleId = 'movie-details-title'
   const castSliderNavigationId = 'movie-card-slider-navigation'
+
+  const castItems = [
+    {
+      imgSrc: '/src/assets/images/people/cast-1.jpg',
+      imgAlt: 'Ivan Ivanov',
+    },
+    {
+      imgSrc: '/src/assets/images/people/cast-2.jpg',
+      imgAlt: 'Ivan Ivanov',
+    },
+    {
+      imgSrc: '/src/assets/images/people/cast-3.jpg',
+      imgAlt: 'Ivan Ivanov',
+    },
+    {
+      imgSrc: '/src/assets/images/people/cast-4.jpg',
+      imgAlt: 'Ivan Ivanov',
+    },
+    {
+      imgSrc: '/src/assets/images/people/cast-5.jpg',
+      imgAlt: 'Ivan Ivanov',
+    },
+    {
+      imgSrc: '/src/assets/images/people/cast-6.jpg',
+      imgAlt: 'Ivan Ivanov',
+    },
+    {
+      imgSrc: '/src/assets/images/people/cast-7.jpg',
+      imgAlt: 'Ivan Ivanov',
+    },
+    {
+      imgSrc: '/src/assets/images/people/cast-8.jpg',
+      imgAlt: 'Ivan Ivanov',
+    },
+  ]
+
+  const reviewItems = [
+    {
+      name: 'Aniket Roy',
+      subtitle: 'From India',
+      description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+      ratingValue: 4.5,
+    },
+    {
+      name: 'Aniket Roy',
+      subtitle: 'From India',
+      description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+      ratingValue: 4.5,
+    },
+    {
+      name: 'Aniket Roy',
+      subtitle: 'From India',
+      description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+      ratingValue: 4.5,
+    },
+    {
+      name: 'Aniket Roy',
+      subtitle: 'From India',
+      description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+      ratingValue: 4.5,
+    },
+    {
+      name: 'Aniket Roy',
+      subtitle: 'From India',
+      description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+      ratingValue: 4.5,
+    },
+    {
+      name: 'Aniket Roy',
+      subtitle: 'From India',
+      description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+      ratingValue: 4.5,
+    },
+    {
+      name: 'Aniket Roy',
+      subtitle: 'From India',
+      description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+      ratingValue: 4.5,
+    },
+    {
+      name: 'Aniket Roy',
+      subtitle: 'From India',
+      description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+      ratingValue: 4.5,
+    },
+  ]
 
   return (
     <section
@@ -47,7 +136,7 @@ const MovieDetails = (props) => {
             sliderParams={{
               slidesPerView: 'auto',
               spaceBetween: 10,
-              breakpoints:{
+              breakpoints: {
                 1024: {
                   slidesPerView: 'auto',
                   spaceBetween: 20,
@@ -56,12 +145,49 @@ const MovieDetails = (props) => {
               },
             }}
           >
-            {}
+            {castItems.map((castItem, index) => (
+              <PersonCard {...castItem} key={index} />
+            ))}
           </Slider>
-
         </div>
 
-        <div className='movie-details__panel'></div>
+        <div className='movie-details__panel'>
+          <header className='movie-details__panel-header'>
+            <h3 className='movie-details__title'>Reviews</h3>
+            <Button
+              mode='black-08'
+              iconName='plus'
+              label='Add Your Review'
+              href='/'
+            />
+          </header>
+          <Slider
+            navigationMode='rounded'
+            isNavigationHiddenMobile={false}
+            hasScrollbarOnMobile={false}
+            sliderParams={{
+              slidesPerView: 2,
+              slidesPerGroup: 2,
+              breakpoints: {
+                0: {
+                  slidesPerView: 1,
+                  slidesPerGroup: 1,
+                  spaceBetween: 16,
+                },
+                1024: {
+                  slidesPerView: 2,
+                  slidesPerGroup: 2,
+                  spaceBetween: 20,
+                  allowTouchMove: false,
+                },
+              },
+            }}
+          >
+            {reviewItems.map((reviewItem, index) => (
+              <ReviewCard {...reviewItem} key={index} />
+            ))}
+          </Slider>
+        </div>
 
       </div>
 
