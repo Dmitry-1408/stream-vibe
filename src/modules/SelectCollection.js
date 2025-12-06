08d19db5d98c8e7fb33490354359b266f48c6b6a
Dmitry Bykov <bykovdmnic@outlook.com>
2025-12-06 17:15:03 +0300

    продолжил Select JS

--- src/modules/SelectCollection.js
+++ src/modules/SelectCollection.js
@@ -28,10 +28,10 @@ class Select extends BaseComponent {
   constructor(rootElement) {
     super()
     this.rootElement = rootElement
-    this.originalControlElement=this.rootElement.querySelector(this.selectors.originalControl)
-    this.buttonElement=this.rootElement.querySelector(this.selectors.button)
-    this.dropdownElement=this.rootElement.querySelector(this.selectors.dropdown)
-    this.optionElements=this.dropdownElement.querySelectorAll(this.selectors.option)
+    this.originalControlElement = this.rootElement.querySelector(this.selectors.originalControl)
+    this.buttonElement = this.rootElement.querySelector(this.selectors.button)
+    this.dropdownElement = this.rootElement.querySelector(this.selectors.dropdown)
+    this.optionElements = this.dropdownElement.querySelectorAll(this.selectors.option)
     this.state = this.getProxyState({
       ...this.initialState,
       currentOptionIndex: this.originalControlElement.selectedIndex,
@@ -39,7 +39,52 @@ class Select extends BaseComponent {
     })
   }
 
+  updateUI() {
+    const {
+      isExpanded,
+      currentOptionIndex,
+      selectedOptionElement,
+    } = this.state
 
+    const newSelectedOptionValue = selectedOptionElement.textContent.trim()
+
+    const updateOriginalControl = () => {
+      this.originalControlElement.value = newSelectedOptionValue
+    }
+
+    const updateButton = () => {
+      this.buttonElement.textContent = newSelectedOptionValue
+      this.buttonElement.classList.toggle(this.stateClasses.isExpanded, isExpanded)
+      this.buttonElement.ariaExpanded = isExpanded
+      this.buttonElement.ariaActiveDescendant = this.optionElements[currentOptionIndex].id
+    }
+
+    const updateDropdown = () => {
+      this.dropdownElement.classList.toggle(this.stateClasses.isExpanded, isExpanded)
+    }
+
+    const updateOptions = () => {
+      this.optionElements.forEach((optionElement, index) => {
+        const isCurrent = currentOptionIndex === index
+        const isSelected = selectedOptionElement === optionElement
+
+        optionElement.classList.toggle(this.stateClasses.isCurrent, isCurrent)
+        optionElement.classList.toggle(this.stateClasses.isSelected, isSelected)
+        optionElement.ariaSelected = isSelected
+      })
+    }
+
+    updateOriginalControl()
+    updateButton()
+    updateDropdown()
+    updateOptions()
+  }
+
+  fixDropdownPosition = () => {
+    const viewportWidth = document.documentElement.clientWidth
+    const viewportCenterX = viewportWidth / 2
+    const {width, x} = this.buttonElement.getBoundingClientRect()
+  }
 }
 
 class SelectCollection {
